dba6662fdab5e411eb12f576ed08e592e30f239a
Clayton Bauman <claytonkb@gmail.com>
2018-11-13 14:20:25 -0800

    Misc changes

--- src/ts.c
+++ src/ts.c
@@ -4,9 +4,10 @@
 #include "cnf.h"
 #include "sls.h"
 
-// Iterative modified DPLL
+
+// Iterative modified DPLL w/UCB
 //
-int ts_solve(babel_env *be, st_state *bs){
+int ts_ucb_solve(babel_env *be, st_state *bs){
 
     int curr_var = 1;
     int result = 0;
@@ -84,17 +85,7 @@ cont_A:
         // else 
 
 // update rewards/num_attempts arrays for first leg
-//      table_select = branch_history[curr_var]
-//      if(curr_assignment == DEC_ASSIGN0_VS)
-//          attempts = bs->attempts_0
-//          rewards = bs->rewards_0
-//      else
-//          attempts = bs->attempts_1
-//          rewards = bs->rewards_1
-//      attempts_table = rdp(attempts, (256*curr_var))
-//      rewards_table  = rdp(rewards,  (256*curr_var))
-//      attempts = rdp(attempts_table, table_select)
-//      rewards  = rdp(rewards_table,  table_select)
+//void ts_ucb_update_stats(st_state *st, var_state curr_assignment, int curr_var){
 
         cnf_var_unassign(bs, curr_var);
 //        if(cnf_var_assign(bs, curr_var, DEC_ASSIGN0_VS)){
@@ -114,13 +105,14 @@ cont_A:
 
 cont_B:
 
-        if(result == 0){ // FIXME: redundant?
+        if(result == 0){
             if(curr_var == 1) // UNSAT
                 break;
             cnf_var_unassign(bs, curr_var);
         }
 
 // update rewards/num_attempts arrays for second leg
+//void ts_ucb_update_stats(st_state *st, var_state curr_assignment, int curr_var){
 
         returning = 1;
         curr_var--;
@@ -132,6 +124,19 @@ cont_B:
     return result;
 
 }
+
+
+//
+//
+var_state ts_rand_assign(void){
+
+    if(sls_mt_rand()%2)
+        return DEC_ASSIGN1_VS;
+    else
+        return DEC_ASSIGN0_VS;
+
+}
+
 
 
 //
@@ -156,12 +161,117 @@ int ts_ucb_choice(float reward_0, float reward_1, float num_attempts_0, float nu
 
 //
 //
-var_state ts_rand_assign(void){
+void ts_ucb_update_stats(st_state *st, var_state curr_assignment, int curr_var){
 
-    if(sls_mt_rand()%2)
-        return DEC_ASSIGN1_VS;
-    else
-        return DEC_ASSIGN0_VS;
+// update rewards/num_attempts arrays for first leg
+//      table_select = branch_history[curr_var]
+//      if(curr_assignment == DEC_ASSIGN0_VS)
+//          attempts = bs->attempts_0
+//          rewards = bs->rewards_0
+//      else
+//          attempts = bs->attempts_1
+//          rewards = bs->rewards_1
+//      attempts_table = rdp(attempts, (256*curr_var))
+//      rewards_table  = rdp(rewards,  (256*curr_var))
+//      attempts = rdp(attempts_table, table_select)
+//      rewards  = rdp(rewards_table,  table_select)
+
+}
+
+
+// Iterative modified DPLL
+//
+int ts_solve(babel_env *be, st_state *bs){
+
+    int curr_var = 1;
+    int result = 0;
+    int returning = 0;
+
+    char *solver_stack = (char*)bs->solver_stack;
+    solver_cont sc;
+
+    while(1){
+
+bs->dev_ctr++;
+if((bs->dev_ctr % 100000) == 0){
+    _prn(".");
+    if((bs->dev_ctr % 100000000) == 0){
+        _say("");
+        _dd(bs->dev_ctr);
+    }
+}
+
+        if(curr_var == 1 && result == 1) // SAT
+             break;
+
+        if(returning){
+             returning = 0;
+             sc = solver_stack[curr_var];
+             switch(sc){
+                 case CONT_A_SC:
+                     goto cont_A;
+                 case CONT_B_SC:
+                     goto cont_B;
+             }
+        }
+
+        if(curr_var > bs->cl->num_variables){
+            if(cnf_clause_all_sat(bs)){
+                result = 1;
+                returning = 1;
+            }
+            curr_var--;
+            continue;
+        }
+
+        if(cnf_var_assign(bs, curr_var, DEC_ASSIGN1_VS)){
+            if(!cnf_var_unsat(bs, curr_var)){
+                 solver_stack[curr_var] = CONT_A_SC;
+                 curr_var++;
+                 continue;
+            }
+        }
+
+cont_A:
+
+        if(result == 1){
+             returning = 1;
+             curr_var--;
+             continue;
+         }
+         // else 
+
+        cnf_var_unassign(bs, curr_var);
+        if(cnf_var_assign(bs, curr_var, DEC_ASSIGN0_VS)){
+
+            if(cnf_var_unsat(bs, curr_var)){
+                result = 0;
+                goto cont_B;
+            }
+            else{
+                solver_stack[curr_var] = CONT_B_SC; 
+                curr_var++;
+                continue;
+            }
+
+        }
+
+cont_B:
+
+        if(result == 0){
+            if(curr_var == 1) // UNSAT
+                break;
+            cnf_var_unassign(bs, curr_var);
+        }
+
+        returning = 1;
+        curr_var--;
+        continue;
+
+
+    }
+
+    return result;
 
 }
 
--- src/ts.h
+++ src/ts.h
@@ -7,9 +7,11 @@
 #ifndef TS_H
 #define TS_H
 
-int ts_solve(babel_env *be, st_state *bs);
-int ts_ucb_choice(float reward_0, float reward_1, float num_attempts_0, float num_attempts_1);
+int  ts_solve(babel_env *be, st_state *bs);
 var_state ts_rand_assign(void);
+int  ts_ucb_solve(babel_env *be, st_state *bs);
+int  ts_ucb_choice(float reward_0, float reward_1, float num_attempts_0, float num_attempts_1);
+void ts_ucb_update_stats(st_state *st, var_state curr_assignment, int curr_var);
 
 
 #endif // TS_H
