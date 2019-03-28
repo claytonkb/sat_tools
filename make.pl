94da7140d06e10bfaacf15356e6d9f6c72f5c837
Clayton Bauman <claytonkb@gmail.com>
2019-03-28 11:33:00 -0700

    Misc changes

--- make.pl
+++ make.pl
@@ -1,116 +1,261 @@
-# make.pl
-#
-# Use this to build zebra on any standard *nix with GCC
-#
-# Use at your own risk. I use Perl instead of autotools because the latter
-#    are an opaque morass of Mad Hatter gibberish.
+#!/usr/bin/perl
+
+use strict;
+
+# Use at your own risk. I use Perl instead of autotools because I loathe
+#    everything about auto-tools
 #
 #   USAGE:
 #
 #   Build all:
+#       ./make.pl
+#       OR
 #       perl make.pl
 #
 #   Clean:
 #       perl make.pl clean
-#$verbose=1;
-$optimize=0;
 
-unless ($#ARGV > -1) {
+use Getopt::Std;
+use IPC::Cmd qw[can_run run run_forked];
+use Cwd;
+use File::Basename;
+use File::stat;
+use Time::localtime;
+use Data::Dumper;
+
+my $project = "sat_tools";
+my $project_dir = cwd();
+my $parent_dir  = $project_dir;
+   $parent_dir  =~ s/\/[^\/]+$//;
+
+my $claytonkb_deps = [ qw{lib_babel} ];
+
+our ($opt_o, $opt_v, $opt_V, $opt_s, $opt_t);
+getopts('ovVst');
+
+my $verbose              = ($opt_v or $opt_V);
+my $recursive_verbose    = "";
+   $recursive_verbose    = "-V " if $opt_V;
+
+# global defaults
+my $default_optimize     = "";
+   $default_optimize     = "-O3" if $opt_o;
+
+my $default_warnings     = "-Wall -Wfatal-errors";
+my $default_libraries    = "-lm -lncurses -lpthread -lrt -l$project";
+
+makepl_cd($parent_dir);
+
+my $parent_sibling_dirs = (makepl_do("ls"))->[0];
+my @parent_sibling_dirs = split /\n/, $parent_sibling_dirs;
+
+for my $dep (@{$claytonkb_deps}){
+    if( ! grep(/^$dep$/, @parent_sibling_dirs) ){
+        makepl_say("Missing claytonkb dependency: $dep");
+        die;
+    }
+}
+
+makepl_cd($project_dir);
+
+my @default_lib_dirs     = ("${project_dir}/lib");
+my @default_include_dirs = ("${project_dir}/src");
+
+push @default_lib_dirs,     "${parent_dir}/$_/lib" for(@{$claytonkb_deps});
+push @default_include_dirs, "${parent_dir}/$_/src" for(@{$claytonkb_deps});
+
+my $default_lib_dirs     = "";
+my $default_include_dirs = "";
+
+$default_lib_dirs       .= "-L$_ " for(@default_lib_dirs);
+$default_include_dirs   .= "-I$_ " for(@default_include_dirs);
+
+my $launch_timestamp     = time();
+my $success=0;
+
+my $arg = shift;
+
+if($arg    eq "clean"){
+    clean();
+}
+elsif($arg eq "deps"){
+    deps();
+}
+elsif($arg eq "libs"){
+    libs();
+}
+elsif($arg eq "all"){
     make_all();
 }
 else{
-    if($ARGV[0] eq "clean"){
-        clean();
-    }
-    elsif($ARGV[0] eq "clean_all"){
-        clean_all();
-    }
-    elsif($ARGV[0] eq "lib_babel"){
-        lib_babel();
-    }
-    elsif($ARGV[0] eq "all_libs"){
-        all_libs();
-    }
-    elsif($ARGV[0] eq "libs"){
-        libs();
-    }
-    elsif($ARGV[0] eq "all"){
-        make_all();
-    }
+    make_all();
 }
 
+makepl_do("touch .last_built"); # XXX if($success) XXX
+
+makepl_say("Done") if $verbose;
+
+# make everything
 sub make_all{
-    print "make_all\n" if $verbose;
-    all_libs();
-    build();
-}
 
-sub all_libs{
-    print "all_libs\n" if $verbose;
-    `mkdir -p lib`;
-    lib_babel();
+    makepl_say("make_all()") if $verbose;
+
+    deps();
     libs();
+    build();
+
 }
 
-sub lib_babel{
-    if(not -e "lib/libbabel.a"){
-        print "lib_babel\n" if $verbose;
-        chdir "lib_babel";
-        `perl make.pl libs`;
-        chdir "../";
-        `cp lib_babel/lib/libbabel.a lib`;
+
+# build deps
+sub deps{
+
+    makepl_say("deps()") if $verbose;
+
+    for my $dep (@{$claytonkb_deps}){
+        makepl_cd("$parent_dir/$dep");
+        if(!-e ".last_built"){
+           system("perl make.pl $recursive_verbose");
+        }
     }
-    else{
-        print "skipping lib_babel\n" if $verbose;
-    }
+
+    makepl_cd("$project_dir");
 }
+
 
 sub libs{
-    print "libs\n" if $verbose;
-#    `mkdir -p lib`;
-    chdir "src";
-    `gcc -O3 -c *.c -I../lib_babel/src -Wfatal-errors -lm` if $optimize;
-    `gcc -c *.c -I../lib_babel/src -Wfatal-errors -lm` unless $optimize;
-    `ar rcs libsat_tools.a *.o`;
-    `mv *.o ../lib`;
-    `mv *.a ../lib`;
-    chdir "../";
+
+    makepl_say("libs()") if $verbose;
+
+    makepl_do("mkdir -p lib");
+
+    makepl_cd("src");
+
+    my $optimize     = "$default_optimize";
+    my $warnings     = "$default_warnings";
+    my $libraries    = "$default_libraries";
+
+    my $include_dirs = "$default_include_dirs";
+    my $lib_dirs     = "$default_lib_dirs";
+
+    my $build_cmd    = "gcc -c *.c $optimize $warnings $libraries "
+                       . "$include_dirs $lib_dirs";
+
+    makepl_do( $build_cmd );
+
+    makepl_do("ar rcs lib$project.a *.o");
+
+    if($opt_s){
+        makepl_do("mkdir -p $project_dir/obj");
+        makepl_do("mv *.o $project_dir/obj");
+    }
+    else{
+        makepl_do("rm *.o");
+    }
+
+    makepl_do("mv *.a $project_dir/lib");
+
+    makepl_cd("$project_dir");
+
 }
 
+
+# build test
 sub build{
-    print "build\n" if $verbose;
-    `mkdir -p bin`;
-    my @libs = `ls lib`;
+
+    makepl_say("build()") if $verbose;
+
+    makepl_do("mkdir -p bin");
+
+    my $optimize     = "$default_optimize";
+    my $warnings     = "$default_warnings";
+    my $libraries    = "$default_libraries";
+
+    my $include_dirs = "$default_include_dirs";
+    my $lib_dirs     = "$default_lib_dirs";
+
+    my $lib_string   = get_libs();
+
+    my $build_string = "gcc test/main.c $lib_string $optimize $warnings "
+                       . "$libraries $include_dirs $lib_dirs -o bin/test";
+
+    makepl_do($build_string);
+
+}
+
+
+# build lib_string
+sub get_libs{
+
     my $lib_string = "";
-    for(@libs){ chomp $_; $lib_string .= "lib/$_ " };
 
-    my $build_string;
-    $build_string =
-        "gcc -O3 test/main.c $lib_string -Llib -lbabel -Isrc -Ilib_babel/src -o bin/test -Wfatal-errors -lm"
-        if $optimized;
+    foreach my $dir (@default_lib_dirs){
 
-    $build_string =
-        "gcc test/main.c $lib_string -Llib -lbabel -Isrc -Ilib_babel/src -o bin/test -Wfatal-errors -lm"
-        unless $optimized;
+        #my $libs = makepl_do("ls $dir");
+        my $libs = `ls $dir/`;
+        my @libs = split(/\n/, $libs);
 
-    `$build_string`;
+        foreach(@libs){ 
+            chomp $_;
+            $lib_string .= "$dir/$_ "
+        }
+
+    }
+
+    return $lib_string;
 
 }
 
-sub clean_all{
-    print "clean_all\n" if $verbose;
-    chdir "lib_babel";
-    `perl make.pl clean`;
-#    chdir "../cnf_parse";
-#    `perl make.pl clean`;
-    chdir "../";
-    clean();
-}
 
+# clean up build directory
 sub clean{
-    print "clean\n" if $verbose;
-    `rm -rf lib`;
-    `rm -rf bin`;
+
+    makepl_say("clean()") if $verbose;
+    makepl_do("rm -rf lib");
+    makepl_do("rm -rf bin");
+    makepl_do("rm -rf obj");
+    makepl_do("rm .last_built");
+
 }
 
+
+
+# run a command
+sub makepl_do {
+
+    my $cmd = shift;
+    makepl_say("$cmd") if $verbose;
+
+    return if $opt_t; # testing switch set
+
+    my ($success, $error_message, $full_buf, $stdout_buf, $stderr_buf  ) = 
+        run( command => $cmd, verbose => 0 );    
+
+    if(!$success){
+        makepl_say("Failure detected, stopping...");
+        print "$error_message\n";
+        print "@{$stderr_buf}\n";
+        die;
+    }
+
+    return $stdout_buf;
+
+}
+
+# print a message
+sub makepl_say {
+    print "MAKE.PL($project): $_[0]\n";
+}
+
+
+sub makepl_cd {
+
+    unless( chdir($_[0]) ){
+        makepl_say("Error changing to directory: $_[0]");
+    }
+
+    my $current_dir = cwd();
+    makepl_say("Current directory: $current_dir") if $verbose;
+
+}
 
