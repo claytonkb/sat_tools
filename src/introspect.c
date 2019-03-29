0edfb295958b6d49b0da4f53bc5b85c276f1bd91
Clayton Bauman <claytonkb@gmail.com>
2019-03-29 15:40:34 -0700

    Added introspect.*

---
+++ src/introspect.c
@@ -0,0 +1,18 @@
+// introspect.c
+
+#include "introspect.h"
+
+//typedef struct {
+//    uint64_t *variables;
+//    uint64_t *clauses;
+//    uint8_t  *clause_lengths;
+//    int       num_variables;
+//    int       num_clauses;
+//    int       num_assignments;
+//} clause_list;
+
+
+
+
+// Clayton Bauman 2018
+
---
+++ src/introspect.h
@@ -0,0 +1,15 @@
+// kca.h
+//
+
+#include "sat_tools.h"
+
+#ifndef INTROSPECT_H
+#define INTROSPECT_H
+
+
+
+#endif // INTROSPECT_H
+
+
+// Clayton Bauman 2018
+
