bd228925933a7d6abb6c5ddd8147a6c422e20c2e
Clayton Bauman <claytonkb@gmail.com>
2019-04-05 11:33:42 -0700

    Misc changes

--- src/introspect_sat.c
+++ src/introspect_sat.c
@@ -34,11 +34,15 @@ str introspect_sat_gv(babel_env *be, st_state *st, clause_list *cl){
     bsprintf(be, result, &str_offset, "edge [style=bold, weight=100];\n");
     bsprintf(be, result, &str_offset, "overlap=scale;\n");
 
+//    bsprintf(be, result, &str_offset, "subgraph variables {\n");
     for(i=0;i<cl->num_variables;i++)
         bsprintf(be, result, &str_offset, "var_x%d [style=bold,shape=record,color=magenta,label=\"<f0> x%d|<f1> ~x%d\"]\n", i+1, i+1, i+1);
+//    bsprintf(be, result, &str_offset, "}\n");
 
+//    bsprintf(be, result, &str_offset, "subgraph clauses {\n");
     for(i=0;i<cl->num_clauses;i++)
         bsprintf(be, result, &str_offset, "cls_%d [style=bold,shape=oval,color=cyan]\n", i);
+//    bsprintf(be, result, &str_offset, "}\n");
 
     mword *clause;
     int tempi;
@@ -48,10 +52,10 @@ str introspect_sat_gv(babel_env *be, st_state *st, clause_list *cl){
         for(j=0;j<size(clause);j++){
             tempi = rdv(clause,j);
             if(tempi<0){
-                bsprintf(be, result, &str_offset, "var_x%d:f0 -- cls_%d\n", abs(tempi), i);
+                bsprintf(be, result, &str_offset, "var_x%d:f0 -- cls_%d [minlen=5]\n", abs(tempi), i);
             }
             else{
-                bsprintf(be, result, &str_offset, "var_x%d:f1 -- cls_%d\n", tempi, i);
+                bsprintf(be, result, &str_offset, "var_x%d:f1 -- cls_%d [minlen=5]\n", tempi, i);
             }
         }
     }
