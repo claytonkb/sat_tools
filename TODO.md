ec59dc92d276488f3225d5b9bc4469f3d641e19d
Clayton Bauman <claytonkb@gmail.com>
2019-04-05 16:01:40 -0700

    Misc changes

--- TODO.md
+++ TODO.md
@@ -4,7 +4,13 @@ sat_tools
 *BUGS AT BOTTOM*
 
     Next:
-        Implement cnf_introspect(); see work/test.dot
+        (DONE) Implement cnf_introspect(); see work/test.dot
+
+        Problem: The Graphviz layout is not very useful on uniform-random graphs.
+        Implement introspect_sat_svg() for this case. Just formulate the
+        variables as columns, clauses as rows and dump an adjacency matrix to
+        SVG using the introspect_svg() approach from lib_babel.
+
 
 BUGS
 ====
--- src/introspect_sat.c
+++ src/introspect_sat.c
@@ -29,6 +29,72 @@ str introspect_sat_gv(babel_env *be, st_state *st, clause_list *cl){
 
     mword str_offset  = 0;
 
+    bsprintf(be, result, &str_offset, "digraph babel {\nnode [shape=record];\n");
+    bsprintf(be, result, &str_offset, "graph [rankdir = \"LR\"];\n");
+//    bsprintf(be, result, &str_offset, "splines=ortho;\n");
+    
+    bsprintf(be, result, &str_offset, "subgraph variables {\n");
+        bsprintf(be, result, &str_offset, "var_x0  [style=invis]\n");
+        bsprintf(be, result, &str_offset, "var_x00 [style=invis]\n");
+        bsprintf(be, result, &str_offset, "splines=ortho;\n");
+        for(i=0;i<cl->num_variables;i++)
+            bsprintf(be, result, &str_offset, "var_x%d [style=bold,shape=record,label=\"<f0> x%d|<f1> ~x%d\"]\n", i+1, i+1, i+1);
+        bsprintf(be, result, &str_offset, "var_x0 -> var_x00 ");
+        for(i=0;i<cl->num_variables;i++)
+            bsprintf(be, result, &str_offset, "-> var_x%d", i+1);
+        bsprintf(be, result, &str_offset, " [style=invis];\n");
+    bsprintf(be, result, &str_offset, "}\n");
+
+    bsprintf(be, result, &str_offset, "subgraph clauses {\n");
+        bsprintf(be, result, &str_offset, "rank=same;\n");
+        for(i=0;i<cl->num_clauses;i++)
+            bsprintf(be, result, &str_offset, "cls_%d [style=bold,shape=oval]\n", i);
+        bsprintf(be, result, &str_offset, "cls_0 ");
+        for(i=1;i<cl->num_clauses;i++)
+            bsprintf(be, result, &str_offset, "-> cls_%d", i);
+        bsprintf(be, result, &str_offset, " [style=invis];\n");
+    bsprintf(be, result, &str_offset, "}\n");
+
+    mword *clause;
+    int tempi;
+
+    for(i=0;i<size(st->clause_array);i++){
+        clause = rdp(st->clause_array,i);
+        for(j=0;j<size(clause);j++){
+            tempi = rdv(clause,j);
+            if(tempi<0){
+                bsprintf(be, result, &str_offset, "var_x%d:f0 -> cls_%d [constraint=false, style=solid, arrowhead=\"none\"];\n", abs(tempi), i);
+            }
+            else{
+                bsprintf(be, result, &str_offset, "var_x%d:f1 -> cls_%d [constraint=false, style=solid, arrowhead=\"none\"];\n", tempi, i);
+            }
+        }
+    }
+
+    bsprintf(be, result, &str_offset, "}\n");
+
+    array_shrink(be,result,0,str_offset-1,U8_ASIZE);
+//    mword final_size = array8_size(result);
+//    _dd(final_size);
+
+    return result;
+
+}
+
+
+#if 0
+// XXX Use the following command to view:
+//      neato -Tsvg work/test.dot > work/test.svg
+str introspect_sat_gv(babel_env *be, st_state *st, clause_list *cl){
+
+    int i,j;
+
+    int approx_num_lines = cl->num_assignments+cl->num_variables+cl->num_clauses;
+    mword *result = mem_new_str(be, 80*approx_num_lines+500, 0);
+//_dd(80*approx_num_lines+500);
+
+    mword str_offset  = 0;
+
     bsprintf(be, result, &str_offset, "graph babel {\nnode [shape=record];\n");
     bsprintf(be, result, &str_offset, "graph [rankdir = \"LR\"];\n");
     bsprintf(be, result, &str_offset, "edge [style=bold, weight=100];\n");
@@ -65,31 +131,7 @@ str introspect_sat_gv(babel_env *be, st_state *st, clause_list *cl){
     return result;
 
 }
-
-//    bsprintf(be, result, &str_offset, "digraph babel {\n");
-//    bsprintf(be, result, &str_offset, "splines=ortho;\n");
-//    bsprintf(be, result, &str_offset, "graph [rankdir = \"LR\"];\n");
-//    bsprintf(be, result, &str_offset, "node[shape = circle, fontsize=14];\n");
-
-//    bsprintf(be, result, &str_offset, "subgraph variables {\n");
-//    bsprintf(be, result, &str_offset, "var_x1 [style=bold,shape=record,color=magenta,label=\"<f0> x1|<f1> ~x1\"]\n");
-//
-//    for(i=1;i<cl->num_variables;i++){
-//        //"0Z" -> "0A" -> "1A" -> "2A";
-//        bsprintf(be, result, &str_offset, "var_x%d -> var_x%d \n", i, i+1);
-//        bsprintf(be, result, &str_offset, "var_x%d [style=bold,shape=record,color=magenta,label=\"<f0> x%d|<f1> ~x%d\"]\n", i+1, i+1, i+1);
-//    }
-//
-//    bsprintf(be, result, &str_offset, "}\n");
-//
-//    subgraph A {
-//        "0Z" [style=invis];
-//        "0Z" -> "0A" -> "1A" -> "2A";
-//        "2A" -> "0A" [label=".", constraint=false, style=solid];
-//    }
-
-//    bsprintf(be, result, &str_offset, "subgraph clauses {\n");
-//    bsprintf(be, result, &str_offset, "}\n");
+#endif
 
 // Clayton Bauman 2018
 
